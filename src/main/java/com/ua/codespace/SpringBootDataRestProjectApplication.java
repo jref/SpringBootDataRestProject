8d2c74ad39719e74bdf3e0cae3b2820362c1f2f9
Your Name <you@example.com>
2017-05-22 00:12:25 +0300

    Initial commit

---
+++ src/main/java/com/ua/codespace/SpringBootDataRestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ua.codespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataRestProjectApplication.class, args);
+	}
+}
